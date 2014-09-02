84cef7a5198d2db55fab7139f94a3ce3752dfa83
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-09-02 16:29:58 -0700

    poly id

---
+++ sql_setup/cicos_2013_poly_id.sql
@@ -0,0 +1,58 @@
+-- This updates the polygon IDs for the cicos columns that determine what polygon
+-- a given point is inside.
+
+UPDATE cicos_2013
+SET gaul_2014_adm0 = (
+	SELECT g.id
+	FROM gaul_2014_adm0 g
+	WHERE ST_Contains(g.geom, cicos_2013.geom)
+	AND g.adm0_name IN ('Tanzania', 'Uganda', 'Nigeria', 'Bangladesh', 'Kenya')
+);
+
+UPDATE cicos_2013
+SET gaul_2014_adm1 = (
+	SELECT g.id
+	FROM gaul_2014_adm1 g
+	WHERE ST_Contains(g.geom, cicos_2013.geom)
+	AND g.adm0_name IN ('Tanzania', 'Uganda', 'Nigeria', 'Bangladesh', 'Kenya')
+);
+
+UPDATE cicos_2013
+SET gaul_2014_adm2 = (
+	SELECT g.id
+	FROM gaul_2014_adm2 g
+	WHERE ST_Contains(g.geom, cicos_2013.geom)
+	AND g.adm0_name IN ('Tanzania', 'Uganda', 'Nigeria', 'Bangladesh', 'Kenya')
+);
+
+UPDATE cicos_2013
+SET nigeria_regions_adm1 = (
+	SELECT g.id
+	FROM nigeria_regions_adm1 g
+	WHERE ST_Contains(g.geom, cicos_2013.geom)
+	AND g.adm0_name IN ('Nigeria')
+);
+
+UPDATE cicos_2013
+SET nigeria_regions_adm2 = (
+	SELECT g.id
+	FROM nigeria_regions_adm2 g
+	WHERE ST_Contains(g.geom, cicos_2013.geom)
+	AND g.adm0_name IN ('Nigeria')
+);
+
+UPDATE cicos_2013
+SET tanzania_regions_adm1 = (
+	SELECT g.id
+	FROM tanzania_regions_adm1 g
+	WHERE ST_Contains(g.geom, cicos_2013.geom)
+	AND g.adm0_name IN ('Tanzania')
+);
+
+UPDATE cicos_2013
+SET tanzania_regions_adm2 = (
+	SELECT g.id
+	FROM tanzania_regions_adm2 g
+	WHERE ST_Contains(g.geom, cicos_2013.geom)
+	AND g.adm0_name IN ('Tanzania')
+);
