10185a32596c5e3a7fec36436f46cefb0ce988b2
Nicholas Hallahan <nhallahan@spatialdev.com>
2014-09-02 14:14:45 -0700

    doing all of the poly tables

--- settings.js
+++ settings.js
@@ -2,7 +2,15 @@ module.exports = {
 
   // Settings for aggregating points into polygons.
   job: {
-    tables: ['gaul_2014_adm0'],
+    tables: [
+      'gaul_2014_adm0',
+      'gaul_2014_adm1',
+      'gaul_2014_adm2',
+      'nigeria_regions_adm1',
+      'nigeria_regions_adm2',
+      'tanzania_regions_adm1',
+      'tanzania_regions_adm2'
+    ],
     points: 'cicos_2013',
     pipeSeparatedValuesFields: ['providers']
   },
